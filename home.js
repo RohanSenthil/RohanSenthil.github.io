ffc3ba7deba14cc4d9c983ef750390ff700182db
RohanSenthil <rohansenthilrs03@gmail.com>
2023-12-15 12:56:55 +0800

    mobile nav

--- home.js
+++ home.js
@@ -5,3 +5,23 @@ for (var i = 0; i < (slices -1); i++) {
   slice.css('transform', 'rotateY(' + angle * i + 'deg)');
   $('.sphere').append(slice);
 }
+
+let menuBtn = document.getElementById('menu-btn');
+let dropdownContent = document.getElementById('nav-dropdown-content');
+let hamburger = document.getElementById('hamburger');
+let closeBtn = document.getElementById('menu-close')
+
+menuBtn.addEventListener('click', () => {
+  let opened = dropdownContent.style.display == 'block';
+
+  if (opened) {
+    dropdownContent.style.display = 'none';
+    closeBtn.style.display = 'none';
+    hamburger.style.display = 'block'
+  } else {
+    hamburger.style.display = 'none'
+    dropdownContent.style.display = 'block';
+    closeBtn.style.display = 'block'
+  }
+
+})
